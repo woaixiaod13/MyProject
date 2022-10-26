efd240fd94428d0bf961007336a75df23e9fd43a
suhaiwen <249223087@qq.com>
2022-10-26 15:13:18 +0800

    default code

---
+++ Source/Surtr/SurtrGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurtrGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURTR_API ASurtrGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
